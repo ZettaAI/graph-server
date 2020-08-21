1fdbc3d9659fab9f351d29485282b20a7df3a048
Akhilesh Halageri <akhileshhalageri@gmail.com>
2020-08-21 09:48:40 +0000

    chore: redundant variable

--- app/middleware.py
+++ app/middleware.py
@@ -15,7 +15,6 @@ class ResponseTimeHeader(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
         start_time = time()
         response = await call_next(request)
-        process_time = time() - start_time
-        response.headers["X-Process-Time"] = str(process_time)
+        response.headers["X-Process-Time"] = str(time() - start_time)
         return response
 
