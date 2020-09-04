5d8a7da7bf18042750484ba3580b85f8bb91cdb1
Akhilesh Halageri <akhileshhalageri@gmail.com>
2020-09-04 13:21:38 +0000

    fix: wrap edit calls with try except

--- app/segmentation/v1/edits.py
+++ app/segmentation/v1/edits.py
@@ -3,6 +3,7 @@ from copy import copy
 
 from fastapi import Request
 from fastapi import APIRouter
+from pychunkedgraph.graph import exceptions
 
 from .edits_helpers import format_edit_result
 from ...utils import get_cg
@@ -16,11 +17,13 @@ async def merge(
 ):
     from .edits_helpers import merge_helper
 
-    # TODO wrap edits with try except
-
-    return format_edit_result(
-        await merge_helper(copy(get_cg(graph_id)), request), int64_as_str
-    )
+    try:
+        result = await merge_helper(copy(get_cg(graph_id)), request)
+    except exceptions.PreconditionError as e:
+        raise exceptions.BadRequest(e)
+    except Exception as e:
+        raise exceptions.InternalServerError(e)
+    return format_edit_result(result, int64_as_str)
 
 
 @router.post("/{graph_id}/split")
@@ -29,9 +32,13 @@ async def split(
 ):
     from .edits_helpers import split_helper
 
-    return format_edit_result(
-        await split_helper(copy(get_cg(graph_id)), request), int64_as_str
-    )
+    try:
+        result = await split_helper(copy(get_cg(graph_id)), request)
+    except exceptions.PreconditionError as e:
+        raise exceptions.BadRequest(e)
+    except Exception as e:
+        raise exceptions.InternalServerError(e)
+    return format_edit_result(result, int64_as_str)
 
 
 @router.post("/{graph_id}/graph/split_preview")
@@ -40,9 +47,14 @@ async def split_preview(
 ):
     from .edits_helpers import split_preview_helper
 
-    return await split_preview_helper(
-        copy(get_cg(graph_id)), request, int64_as_str=int64_as_str
-    )
+    try:
+        return await split_preview_helper(
+            copy(get_cg(graph_id)), request, int64_as_str=int64_as_str
+        )
+    except exceptions.PreconditionError as e:
+        raise exceptions.BadRequest(e)
+    except Exception as e:
+        raise exceptions.InternalServerError(e)
 
 
 @router.post("/{graph_id}/undo")
@@ -51,9 +63,13 @@ async def undo(
 ):
     from .edits_helpers import undo_helper
 
-    return format_edit_result(
-        await undo_helper(copy(get_cg(graph_id)), request), int64_as_str
-    )
+    try:
+        result = await undo_helper(copy(get_cg(graph_id)), request)
+    except exceptions.PreconditionError as e:
+        raise exceptions.BadRequest(e)
+    except Exception as e:
+        raise exceptions.InternalServerError(e)
+    return format_edit_result(result, int64_as_str)
 
 
 @router.post("/{graph_id}/redo")
@@ -62,6 +78,11 @@ async def redo(
 ):
     from .edits_helpers import redo_helper
 
-    return format_edit_result(
-        await redo_helper(copy(get_cg(graph_id)), request), int64_as_str
-    )
+    try:
+        result = await redo_helper(copy(get_cg(graph_id)), request)
+    except exceptions.PreconditionError as e:
+        raise exceptions.BadRequest(e)
+    except Exception as e:
+        raise exceptions.InternalServerError(e)
+    return format_edit_result(result, int64_as_str)
+
